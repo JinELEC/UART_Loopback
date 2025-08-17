dd519fa3d9d39f10919efce3e07f5429baaf7d17
Jin_HARDWARE <ljkrhs3025@gmail.com>
2025-08-17 23:07:08 +0900

    Update uart_rx.v

--- src/uart_rx.v
+++ src/uart_rx.v
@@ -1,50 +1,43 @@
 module uart_rx(
     input           mclk,
     input           n_reset,
-    input   [15:0]  baudrate,
-    input   [1:0]   parity_sel,
-    input           stop_sel,
-    input           rd_en,
-    output  [7:0]   rdata,
-    output          rvalid,     
-    output          overrun,    // fifo full 
-    output  reg     frame_err,  // stop bit error
-    output  reg     parity_err, // parity error
+    input   [15:0]  baud_max_cnt,
+    input   [1:0]   parity_sel, // 0: none, 1: even, 2: odd
+    input           stop_sel,   // 0: 1 stop bit, 1: 2 stop bit
+    input           read_en,
+    output  [7:0]   rd_data,
+    output          rd_valid,
+    output          full,
+    output  reg     frame_err,
+    output  reg     parity_err,
     input           rxd
 );
 
 // Define states
-reg [1:0] present_state, next_state;
+reg [1:0] state;
 parameter IDLE    = 2'd0;
 parameter RECEIVE = 2'd1;
 parameter DONE    = 2'd2;
 
 // State flag
-wire idle_flag    = (present_state == IDLE)    ? 1'b1 : 1'b0;
-wire receive_flag = (present_state == RECEIVE) ? 1'b1 : 1'b0;
-wire done_flag    = (present_state == DONE)    ? 1'b1 : 1'b0;
-
-// State update
-always@(negedge n_reset, posedge mclk)
-    if(!n_reset)
-        present_state <= IDLE;
-    else
-        present_state <= next_state;
+wire idle_flag    = (state == IDLE)    ? 1'b1 : 1'b0;
+wire receive_flag = (state == RECEIVE) ? 1'b1 : 1'b0;
+wire done_flag    = (state == DONE)    ? 1'b1 : 1'b0;
 
 // State transition
-always@(*) begin
-    next_state = present_state;
-    case(present_state)
-        IDLE    : next_state = (idle_flag & rxd_negedge)                                                           ? RECEIVE : IDLE;
-        RECEIVE : next_state = ((parity_sel == 2'b00) & (stop_sel == 1'b0) & (cnt2 == 4'd9)  & (cnt1 == baudrate)) ? DONE : 
-                               ((parity_sel == 2'b00) & (stop_sel == 1'b1) & (cnt2 == 4'd10) & (cnt1 == baudrate)) ? DONE :
-                               ((parity_sel != 2'b00) & (stop_sel == 1'b0) & (cnt2 == 4'd10) & (cnt1 == baudrate)) ? DONE :
-                               ((parity_sel != 2'b00) & (stop_sel == 1'b1) & (cnt2 == 4'd11) & (cnt1 == baudrate)) ? DONE : RECEIVE;
-       DONE     : next_state = (done_flag & (done_cnt == 3'd7))                                                    ? IDLE : DONE;
-    endcase
-end
+always@(negedge n_reset, posedge mclk)
+    if(!n_reset)
+        state <= 2'b0;
+    else
+        state <= (idle_flag & rxd_negedge)                                                               ? RECEIVE :
+                 ((parity_sel == 2'b00) & (stop_sel == 1'b0) & (cnt2 == 4'd9)  & (cnt1 == baud_max_cnt)) ? DONE :
+                 ((parity_sel == 2'b00) & (stop_sel == 1'b1) & (cnt2 == 4'd10) & (cnt1 == baud_max_cnt)) ? DONE :
+                 ((parity_sel != 2'b00) & (stop_sel == 1'b0) & (cnt2 == 4'd10) & (cnt1 == baud_max_cnt)) ? DONE :
+                 ((parity_sel != 2'b00) & (stop_sel == 1'b1) & (cnt2 == 4'd11) & (cnt1 == baud_max_cnt)) ? DONE :
+                 (done_flag & (done_cnt == 3'd7))                                                        ? IDLE : state;
 
-// rxd negative edge 
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
+// rxd negative edge
 reg rxd_1d, rxd_2d, rxd_3d;
 wire rxd_negedge = ~rxd_2d & rxd_3d;
 always@(negedge n_reset, posedge mclk)
@@ -65,8 +58,8 @@ always@(negedge n_reset, posedge mclk)
     if(!n_reset)
         cnt1 <= 16'b0;
     else
-        cnt1 <= (~receive_flag)    ? 16'b0 :
-                (cnt1 == baudrate) ? 16'b0 : cnt1 + 1'b1;
+        cnt1 <= (~receive_flag) ? 4'b0 :
+                (cnt1 == baud_max_cnt) ? 16'b0 : cnt1 + 1'b1;
 
 // transmitting bit counter
 reg [3:0] cnt2;
@@ -74,26 +67,26 @@ always@(negedge n_reset, posedge mclk)
     if(!n_reset)
         cnt2 <= 4'b0;
     else
-        cnt2 <= (~receive_flag)    ? 4'b0 :
-                (cnt1 == baudrate) ? cnt2 + 1'b1 : cnt2;
+        cnt2 <= (~receive_flag) ? 4'b0 :
+                (cnt1 == baud_max_cnt) ? cnt2 + 1'b1 : cnt2;
 
-// rxd data field part
+// read data from rxd
 reg [7:0] rxd_data;
 always@(negedge n_reset, posedge mclk)
     if(!n_reset)
         rxd_data <= 8'b0;
     else begin
-        rxd_data[0] <= ((cnt2 == 4'd1) & (cnt1 == {1'b0, baudrate[15:1]})) ? rxd_3d : rxd_data[0];
-        rxd_data[1] <= ((cnt2 == 4'd2) & (cnt1 == {1'b0, baudrate[15:1]})) ? rxd_3d : rxd_data[1];
-        rxd_data[2] <= ((cnt2 == 4'd3) & (cnt1 == {1'b0, baudrate[15:1]})) ? rxd_3d : rxd_data[2];
-        rxd_data[3] <= ((cnt2 == 4'd4) & (cnt1 == {1'b0, baudrate[15:1]})) ? rxd_3d : rxd_data[3];
-        rxd_data[4] <= ((cnt2 == 4'd5) & (cnt1 == {1'b0, baudrate[15:1]})) ? rxd_3d : rxd_data[4];
-        rxd_data[5] <= ((cnt2 == 4'd6) & (cnt1 == {1'b0, baudrate[15:1]})) ? rxd_3d : rxd_data[5];
-        rxd_data[6] <= ((cnt2 == 4'd7) & (cnt1 == {1'b0, baudrate[15:1]})) ? rxd_3d : rxd_data[6];
-        rxd_data[7] <= ((cnt2 == 4'd8) & (cnt1 == {1'b0, baudrate[15:1]})) ? rxd_3d : rxd_data[7];
+        rxd_data[0] <= ((cnt2 == 4'd1) & (cnt1 == {1'b0, baud_max_cnt[15:1]})) ? rxd_3d : rxd_data[0];
+        rxd_data[1] <= ((cnt2 == 4'd2) & (cnt1 == {1'b0, baud_max_cnt[15:1]})) ? rxd_3d : rxd_data[1];
+        rxd_data[2] <= ((cnt2 == 4'd3) & (cnt1 == {1'b0, baud_max_cnt[15:1]})) ? rxd_3d : rxd_data[2];
+        rxd_data[3] <= ((cnt2 == 4'd4) & (cnt1 == {1'b0, baud_max_cnt[15:1]})) ? rxd_3d : rxd_data[3];
+        rxd_data[4] <= ((cnt2 == 4'd5) & (cnt1 == {1'b0, baud_max_cnt[15:1]})) ? rxd_3d : rxd_data[4];
+        rxd_data[5] <= ((cnt2 == 4'd6) & (cnt1 == {1'b0, baud_max_cnt[15:1]})) ? rxd_3d : rxd_data[5];
+        rxd_data[6] <= ((cnt2 == 4'd7) & (cnt1 == {1'b0, baud_max_cnt[15:1]})) ? rxd_3d : rxd_data[6];
+        rxd_data[7] <= ((cnt2 == 4'd8) & (cnt1 == {1'b0, baud_max_cnt[15:1]})) ? rxd_3d : rxd_data[7];
     end
 
-// calculate parity bit
+// calculate partity bit & parity bit from rxd
 wire cal_parity = rxd_data[0]^rxd_data[1]^rxd_data[2]^rxd_data[3]^rxd_data[4]^rxd_data[5]^rxd_data[6]^rxd_data[7];
 
 reg cal_parity2;
@@ -103,31 +96,30 @@ always@(negedge n_reset, posedge mclk)
     else
         cal_parity2 <= (parity_sel == 2'b01) ? cal_parity : (parity_sel == 2'b10) ? ~cal_parity : 1'b0;
 
-// parity bit from receive data
 reg rxd_parity;
 always@(negedge n_reset, posedge mclk)
     if(!n_reset)
         rxd_parity <= 1'b0;
     else
-        rxd_parity <= ((cnt2 == 4'd9) & (cnt1 == {1'b0, baudrate[15:1]})) ? rxd_3d : rxd_parity;
+        rxd_parity <= ((cnt2 == 4'd9) & (cnt1 == {1'b0, baud_max_cnt[15:1]})) ? rxd_3d : rxd_parity;
 
-// stop_bit1 from rxd
+// stop bit 1
 reg stop_bit1;
 always@(negedge n_reset, posedge mclk)
     if(!n_reset)
         stop_bit1 <= 1'b0;
     else
-        stop_bit1 <= ((parity_sel == 2'b00) & (cnt2 == 4'd9) & (cnt1 == {1'b0, baudrate[15:1]})) ? rxd_3d :
-                     ((parity_sel != 2'b00) & (cnt2 == 4'd10) & (cnt1 == {1'b0, baudrate[15:1]})) ? rxd_3d : stop_bit1;
+        stop_bit1 <= ((parity_sel == 2'b00) & (cnt2 == 4'd9)  & (cnt1 == {1'b0, baud_max_cnt[15:1]})) ? rxd_3d :
+                     ((parity_sel != 2'b00) & (cnt2 == 4'd10) & (cnt1 == {1'b0, baud_max_cnt[15:1]})) ? rxd_3d : stop_bit1;
 
-// stop_bit2 from rxd
+// stop bit 2
 reg stop_bit2;
 always@(negedge n_reset, posedge mclk)
     if(!n_reset)
         stop_bit2 <= 1'b0;
     else
-        stop_bit2 <= ((parity_sel == 2'b00) & (cnt2 == 4'd10) & (cnt1 == {1'b0, baudrate[15:1]})) ? rxd_3d :
-                     ((parity_sel != 2'b00) & (cnt2 == 4'd11) & (cnt1 == {1'b0, baudrate[15:1]})) ? rxd_3d : stop_bit2;
+        stop_bit2 <= ((parity_sel == 2'b00) & (cnt2 == 4'd10) & (cnt1 == {1'b0, baud_max_cnt[15:1]})) ? rxd_3d :
+                     ((parity_sel != 2'b00) & (cnt2 == 4'd11) & (cnt1 == {1'b0, baud_max_cnt[15:1]})) ? rxd_3d : stop_bit2;
 
 // done state counter
 reg [2:0] done_cnt;
@@ -136,15 +128,6 @@ always@(negedge n_reset, posedge mclk)
         done_cnt <= 3'b0;
     else
         done_cnt <= (done_flag) ? done_cnt + 1'b1 : 3'b0;
-
-// errors
-// frame_err
-always@(negedge n_reset, posedge mclk)
-    if(!n_reset)
-        frame_err <= 1'b0;
-    else
-        frame_err <= ((done_cnt == 3'd5) & (stop_sel == 1'b0) & (stop_bit1 == 1'b0)) ? 1'b1 : 
-                     ((done_cnt == 3'd5) & (stop_sel == 1'b1) & ((stop_bit1 == 1'b0) | (stop_bit2 == 1'b0))) ? 1'b1 : frame_err;
 
 // parity bit error
 always@(negedge n_reset, posedge mclk)
@@ -155,28 +138,37 @@ always@(negedge n_reset, posedge mclk)
                       ((done_cnt == 3'd5) & (cal_parity2 == rxd_parity)) ? 1'b0 :
                       ((done_cnt == 3'd5) & (cal_parity2 != rxd_parity)) ? 1'b1 : parity_err;
 
-wire   fifo_full;
-assign overrun = fifo_full;
+// frame error
+always@(negedge n_reset, posedge mclk)
+    if(!n_reset)
+        frame_err <= 1'b0;
+    else
+        frame_err <= ((done_cnt == 3'd5) & (stop_sel == 1'b0) & (stop_bit1 == 1'b0)) ? 1'b1 :
+                     ((done_cnt == 3'd5) & (stop_sel == 1'b1) & (stop_bit1 == 1'b0 | stop_bit2 == 1'b0)) ? 1'b1 : frame_err;
+    
+// fifo ports
+wire fifo_full;
+assign full = fifo_full;
 
-wire   fifo_empty;
-assign rvalid = ~fifo_empty;
+wire fifo_empty;
+assign rd_valid = ~fifo_empty;
 
-wire   [7:0] fifo_din = rxd_data;
-wire   fifo_wen = (done_cnt == 3'd2) ? 1'b1 : 1'b0;
-wire   fifo_ren = rd_en;
-wire   [7:0] fifo_dout;
-assign  rdata = fifo_dout;
+wire [7:0] fifo_din = rxd_data;
+wire fifo_write_en = (done_cnt == 3'd2) ? 1'b1 : 1'b0;
+wire fifo_read_en = read_en;
+wire [7:0] fifo_dout;
+assign rd_data = fifo_dout;
 
-fifo_16x8 rxd_fifo(
-    .clk            (mclk),
-    .srst           (1'b0),
-    .din            (fifo_din),
-    .wr_en          (fifo_wen),
-    .rd_en          (fifo_ren),
-    .dout           (fifo_dout),
-    .full           (fifo_full),
-    .empty          (fifo_empty),
-    .valid          ()
+fifo_16x8 t0(
+    .clk                (mclk),
+    .srst               (1'b0),
+    .din                (fifo_din),
+    .wr_en              (fifo_write_en),
+    .rd_en              (fifo_read_en),
+    .dout               (fifo_dout),
+    .full               (fifo_full),
+    .empty              (fifo_empty),
+    .valid              ()
 );
 
 endmodule
